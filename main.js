0a6a21ef1de3eef0645d962d9f99e765130aab2d
Stalin Acurio <sta_vla411@hotmail.com>
2022-09-04 21:45:57 -0500

    temporarily teminated

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-function changeIconArrow(selector, nameAttribute) {
-    if (selector.getAttribute(nameAttribute) == './images/icon-arrow-down.svg') {
-        selector.setAttribute(nameAttribute, './images/icon-arrow-up.svg')
+function changeIconArrow(selector, nameAttribute,index) {
+    if (selector[index].getAttribute(nameAttribute) == './images/icon-arrow-down.svg') {
+        selector[index].setAttribute(nameAttribute, './images/icon-arrow-up.svg')
     } else {
-        selector.setAttribute(nameAttribute, './images/icon-arrow-down.svg')
+        selector[index].setAttribute(nameAttribute, './images/icon-arrow-down.svg')
     }
 }
+
+const iconChange = document.querySelectorAll(".changeIcon")
 //Open and close the mobile menu
 const iconOpenMobileMenu = document.querySelector(".mobile-menu-icon")
 const iconCloseMobileMenu = document.querySelector(".icon-close-menu")
@@ -14,19 +16,28 @@ iconOpenMobileMenu.addEventListener('click', () => { mobileMenu.classList.toggle
 iconCloseMobileMenu.addEventListener('click', () => { mobileMenu.classList.toggle('inactive') })
 
 //Open and close the features submenu
-const openFeatures = document.querySelector("#open-features")
-const featuresSubmenu = document.querySelector(".features-submenu")
+const openFeatures = document.querySelectorAll(".open-features")
+const featuresSubmenu = document.querySelectorAll(".features-submenu")
 
-openFeatures.addEventListener('click', () => {
-    changeIconArrow(openFeatures, 'src')
-    featuresSubmenu.classList.toggle('inactive')
+openFeatures[0].addEventListener('click', () => {
+    changeIconArrow(iconChange, 'src', 0)
+    featuresSubmenu[0].classList.toggle('inactive')
 })
-
+openFeatures[1].addEventListener('click', () => {
+    changeIconArrow(iconChange, 'src', 2)
+    featuresSubmenu[1].classList.toggle('inactive')
+})
 //Open and close the company submenu
-const openCompany = document.querySelector("#open-company")
-const companySubmenu = document.querySelector('.company-submenu')
+const openCompany = document.querySelectorAll(".open-company")
+const companySubmenu = document.querySelectorAll('.company-submenu')
 
-openCompany.addEventListener('click', () => {
-    changeIconArrow(openCompany, 'src')
-    companySubmenu.classList.toggle('inactive')
+openCompany[0].addEventListener('click', () => {
+    changeIconArrow(iconChange, 'src', 1)
+    companySubmenu[0].classList.toggle('inactive')
 })
+openCompany[1].addEventListener('click', () => {
+    changeIconArrow(iconChange, 'src', 3)
+    companySubmenu[1].classList.toggle('inactive')
+})
+
+console.log(openFeatures)
